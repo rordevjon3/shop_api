1df4ca11784409a191b0359f0b437c30a83a8582
rordevjon3 <rordevjhn33@proton.me>
2020-08-15 10:38:20 -0600

    1. Two fields was added to product model -> image_product and description. 2. The necessary gems were added for uploading the image_product to aws (s3) 3. The annotate gem was added for documentation the models. 4. The ability model was modify.

--- app/models/type_product.rb
+++ app/models/type_product.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: type_products
+#
+#  id                :bigint           not null, primary key
+#  name_type_product :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class TypeProduct < ApplicationRecord
   has_many :products
 
